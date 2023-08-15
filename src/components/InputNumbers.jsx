afa58090f06bc25353dfc7b90abb10627e7627ca
lomsadze123 <beka.lomsadze.1@btu.edu.ge>
2023-08-15 13:52:33 +0400

    age-calculator-app

--- src/components/InputNumbers.jsx
+++ src/components/InputNumbers.jsx
@@ -7,42 +7,82 @@ const InputNumbers = ({ onChange }) => {
   const [month, setMonth] = useState("");
   const [year, setYear] = useState("");
 
+  const currentYear = new Date().getFullYear();
+
+  const calculateAge = () => {
+    const currentDate = new Date();
+    const birthDate = new Date(year, month - 1, day);
+    const timeDiff = currentDate - birthDate;
+
+    const ageDate = new Date(timeDiff);
+    const years = ageDate.getUTCFullYear() - 1970;
+    const months = ageDate.getUTCMonth();
+    const days = ageDate.getUTCDate() - 1;
+
+    return { years, months, days };
+  };
+
   const handleValueChange = () => {
-    onChange(day, month, year);
+    if (
+      day !== "" &&
+      month !== "" &&
+      year !== "" &&
+      day <= 31 &&
+      month <= 12 &&
+      year < currentYear
+    ) {
+      const age = calculateAge();
+      onChange(age.days, age.months, age.years);
+    }
   };
 
   return (
     <Labels>
       <section>
         <div>
-          <label htmlFor="day">day</label> <br />
-          <input
+          <Label about={day > 31} htmlFor="day">
+            day
+          </Label>{" "}
+          <br />
+          <Input
+            about={day > 31}
             onChange={(e) => setDay(e.target.value)}
             type="number"
             id="day"
             placeholder="DD"
             value={day}
           />
+          {day > 31 && <h3>Enter valid day</h3>}
         </div>
         <div>
-          <label htmlFor="month">month</label> <br />
-          <input
+          <Label about={month > 12} htmlFor="month">
+            month
+          </Label>{" "}
+          <br />
+          <Input
+            about={month > 12}
             onChange={(e) => setMonth(e.target.value)}
             type="number"
             id="month"
             placeholder="MM"
             value={month}
           />
+          {month > 12 && <h3>Enter valid month</h3>}
         </div>
         <div>
-          <label htmlFor="year">year</label> <br />
-          <input
+          <Label about={year > currentYear} htmlFor="year">
+            year
+          </Label>{" "}
+          <br />
+          <Input
+            about={year > currentYear}
             onChange={(e) => setYear(e.target.value)}
             type="number"
             id="year"
             placeholder="YYYY"
             value={year}
           />
+          {year >= currentYear && <h3>Enter valid year</h3>}
         </div>
       </section>
       <Button handleValueChange={handleValueChange} />
@@ -57,12 +97,17 @@ const Labels = styled.section`
     display: flex;
     gap: 1.5rem;
   }
-
+  h3 {
+    font-size: 1.2rem;
+    color: #ff5959;
+    font-weight: 500;
+    font-style: italic;
+    margin-top: 0.6rem;
+  }
   input {
     max-width: 8.8rem;
     width: 100%;
     padding: 1.6rem 0 1.6rem 1.9rem;
-    border: 0.1rem solid #dcdcdc;
     border-radius: 0.8rem;
     font-size: 1.8rem;
     outline: 0;
@@ -74,14 +119,10 @@ const Labels = styled.section`
   input::placeholder {
     opacity: 0.8;
   }
-  input:focus {
-    border: 0.1rem solid #854dff;
-  }
   label {
     font-size: 1.5rem;
     text-transform: uppercase;
     letter-spacing: 0.2rem;
-    color: #716f6f;
   }
 
   @media (min-width: 768px) {
@@ -92,5 +133,18 @@ const Labels = styled.section`
     label {
       font-size: 1.6rem;
     }
+    h3 {
+      font-size: 1.6rem;
+    }
   }
+`;
+const Label = styled.label`
+  color: ${(props) => (props.about ? "#ff5959" : "#716f6f")};
+`;
+const Input = styled.input`
+  &:focus {
+    border: 0.1rem solid #854dff;
+  }
+  border: ${(props) =>
+    props.about ? "0.1rem solid #ff5959" : "0.1rem solid #dcdcdc"};
 `;
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,13 +4,13 @@ const Result = ({ years, months, days }) => {
   return (
     <Section>
       <div>
-        <h2>{!!years ? years : "- -"}</h2> <p>years</p>
+        <h2>{!!years || years === 0 ? years : "- -"}</h2> <p>years</p>
       </div>
       <div>
-        <h2>{!!years ? years : "- -"}</h2> <p>months</p>
+        <h2>{!!months || months === 0 ? months : "- -"}</h2> <p>months</p>
       </div>
       <div>
-        <h2>{!!years ? years : "- -"}</h2> <p>days</p>
+        <h2>{!!days || days === 0 ? days : "- -"}</h2> <p>days</p>
       </div>
     </Section>
   );
